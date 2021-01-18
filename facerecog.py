0513852f1c72131d0031c83c4e65d32b52147531
AdithyaAnil31 <63533838+AdithyaAnil31@users.noreply.github.com>
2021-01-18 22:37:45 +0530

    Update facerecog.py

--- facerecog.py
+++ facerecog.py
@@ -1,25 +1,23 @@
-# Importing relevant modules
+# Importing modules
 import PIL.Image
 import PIL.ImageDraw
-import face_recognition
+import face_recognition #must pip install, link in readme 
 
-# Let's start by inputting an image and getting Python
-# to recognise it!
-picture = face_recognition.load_image_file('me.jpg')
-face_locations = face_recognition.face_locations(picture)
-print(face_locations)
+# Reading the image for which the faces has to be detected
+file = face_recognition.load_image_file('face.jpg')
+faces = face_recognition.face_locations(file)
+print(faces)
 
-# We get python to print how many faces it recognises!
-how_many_faces = len(face_locations)
-print("Python located {} face(s) in this image".format(how_many_faces))
+# Now we find the number of faces
+faces_count = len(faces)
+print("There are {} face(s) in this image".format(how_many_faces))
 
-# Get python to draw rectangles around the images!
-pil_picture = PIL.Image.fromarray(picture)
-draw_picture = PIL.ImageDraw.Draw(pil_picture)
+# This here draws rectangles around the face
+picture = PIL.Image.fromarray(file)
+rect_draw = PIL.ImageDraw.Draw(picture)
 
-for faces in face_locations:
-    top, right, bottom, left = faces
-    draw_picture.rectangle([right, bottom, left, top],
+for i in faces:
+    top, right, bottom, left = i
+    rect_draw.rectangle([right, bottom, left, top],
                            outline="green", width=8)
-
 pil_picture.show()
